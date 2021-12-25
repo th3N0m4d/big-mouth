386d05fa62a30868bef5cf7758b5ad5f46910fee
th3N0m4d <edielton.dantas@hotmail.com>
2021-12-25 18:43:14 +0100

    feat: add aws4 authentication for private API

--- functions/home.js
+++ functions/home.js
@@ -2,6 +2,20 @@ import fsPromises from "fs/promises";
 import { default as Mustache } from "mustache";
 import axios from "axios";
 import path from "path";
+import { aws4Interceptor } from "aws4-axios";
+
+const interceptor = aws4Interceptor(
+  {
+    region: process.env.REGION,
+    service: "execute-api",
+  },
+  {
+    accessKeyId: "",
+    secretAccessKey: "",
+  }
+);
+
+axios.interceptors.request.use(interceptor);
 
 const loadTemplate = async () => {
   const templatePath = path.join(__dirname, "..", "static", "index.html");
@@ -10,20 +24,35 @@ const loadTemplate = async () => {
   return html;
 };
 
+const getRestaurants = async () => {
+  const { data } = await axios.get(`${process.env.API}restaurants`);
+
+  return data;
+};
+
 export const index = async () => {
-  const template = await loadTemplate();
-  const { data: restaurants } = await axios.get(
-    `${process.env.API}restaurants`
-  );
-  const dayOfWeek = "Monday";
+  try {
+    const dayOfWeek = "Monday";
+    const template = await loadTemplate();
+    const restaurants = await getRestaurants();
 
-  const html = Mustache.render(template, { dayOfWeek, restaurants });
+    const html = Mustache.render(template, { dayOfWeek, restaurants });
 
-  return {
-    statusCode: 200,
-    body: html,
-    headers: {
-      "Content-Type": "text/html; charset=UTF-8",
-    },
-  };
+    return {
+      statusCode: 200,
+      body: html,
+      headers: {
+        "Content-Type": "text/html; charset=UTF-8",
+      },
+    };
+  } catch (error) {
+    console.log(error.message);
+    return {
+      statusCode: 500,
+      body: `<h1>${error.message}</h1>`,
+      headers: {
+        "Content-Type": "text/html; charset=UTF-8",
+      },
+    };
+  }
 };
